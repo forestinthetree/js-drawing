6d302c0558d81d659ffb8b63cc238c3d161e8a43
Tak Tran <contact@tutaktran.com>
2020-09-23 22:44:49 +0100

    Add svg drawing for pts.js
    * Use SVGSpace
    * Add url query string to select svg renderer
    * Convert animation arrow functions to normal functions, so scope is retained
    * Set `form.scope(this)` within `space.add`
    * Draw stop icon as polygon, as if statement wasn’t working to redraw

--- src/ptsjs/draw.js
+++ src/ptsjs/draw.js
@@ -1,4 +1,6 @@
 /*global Pts:true */
+import { getUrlParameters } from "../utils/get-url-parameters";
+
 import poseToBody from "../drawing/pose-to-body";
 
 import { BODY_PART_GROUPS } from "../drawing/constants";
@@ -8,7 +10,12 @@ import poseExample2 from "../data/poseExample2";
 
 import { createUpdateGenerator } from "../data/create-update-generator";
 
-const { Polygon, CanvasSpace, Pt, Triangle, Rectangle } = Pts;
+const RENDERER_TYPES = {
+  canvas: "canvas",
+  svg: "svg",
+};
+
+const { Polygon, CanvasSpace, SVGSpace, Pt, Triangle, Rectangle } = Pts;
 
 const getBodyParts = (body) => {
   const bodyParts = {};
@@ -32,7 +39,10 @@ const initUpdate = ({ space, generator }) => {
   generator.start();
 
   const form = space.getForm();
-  space.add(() => {
+  space.add(function () {
+    // Required for SVGForm
+    form.scope && form.scope(this);
+
     const bodyParts = getBodyParts(generator.value);
     Object.values(bodyParts).forEach((bodyPart) => {
       form.stroke("#fff", 4, "round", "round").fill("white").polygons(bodyPart);
@@ -46,15 +56,15 @@ const createPlayControls = ({ space, generators }) => {
   };
   const form = space.getForm();
 
+  const playIcon = Triangle.fromCenter(new Pt(25, 45), 10).rotate2D(33);
+  const stopIcon = Rectangle.polygon(Rectangle.fromCenter(new Pt(17, 35), 17));
   space.add({
-    animate: () => {
-      if (state.isPlaying) {
-        const playIcon = Triangle.fromCenter(new Pt(25, 45), 10).rotate2D(33);
-        form.fill("#fff").polygon(playIcon);
-      } else {
-        const stopIcon = Rectangle.fromCenter(new Pt(17, 35), 17);
-        form.fill("#fff").rect(stopIcon);
-      }
+    animate() {
+      // Required for SVGForm
+      form.scope && form.scope(this);
+
+      const polygon = state.isPlaying ? playIcon : stopIcon;
+      form.fill("#fff").polygon(polygon);
     },
     action: (type) => {
       if (type === "up") {
@@ -72,23 +82,43 @@ const createPlayControls = ({ space, generators }) => {
   return state;
 };
 
-const init = ({ image }) => {
+const init = ({ image, type = RENDERER_TYPES.canvas }) => {
   const { width, height } = image;
-  const canvas = document.createElement("canvas");
-  canvas.width = width;
-  canvas.height = height;
-  canvas.style.cursor = "pointer";
 
-  // Add canvas to page
-  document.body.appendChild(canvas);
+  let space;
+  if (type === RENDERER_TYPES.canvas) {
+    const canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    canvas.style.cursor = "pointer";
 
-  const space = new CanvasSpace(canvas);
+    // Add canvas to page
+    document.body.appendChild(canvas);
+
+    space = new CanvasSpace(canvas);
+  } else if (type === RENDERER_TYPES.svg) {
+    const svgContainer = document.createElement("div");
+    svgContainer.style.width = `${width}px`;
+    svgContainer.style.height = `${height}px`;
+    svgContainer.style.cursor = "pointer";
+
+    // Add svg to page
+    document.body.appendChild(svgContainer);
+
+    space = new SVGSpace(svgContainer);
+  } else {
+    throw new Error(`Invalid renderer type: ${type}`);
+  }
+
   space.setup({ bgcolor: "#000", retina: true });
 
   const form = space.getForm();
+  space.add(function (time, ftime) {
+    // Required for SVGForm
+    form.scope && form.scope(this);
 
-  space.add((time, ftime) => {
     form.fill("white").font(16).text(new Pt(0, 16), "Pts.js");
+    form.fill("white").font(16).text(new Pt(60, 16), type);
   });
 
   space.bindMouse().bindTouch().play();
@@ -97,7 +127,8 @@ const init = ({ image }) => {
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-  const space = init({ image: poseExample.image });
+  const { type } = getUrlParameters(window.location.href);
+  const space = init({ image: poseExample.image, type });
 
   const pose1Generator = createUpdateGenerator({
     body: poseToBody({ pose: poseExample.poses[0] }),
